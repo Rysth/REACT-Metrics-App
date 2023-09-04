cae29b03150dc3ae83b752fd3ff54d4e2f362a07
Rysth <johnpalacios.t@gmail.com>
2023-09-04 11:19:32 -0500

    Created the hero section.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Logo from '../../assets/PNG/logo.png';
 
 function Home() {
   return (
     <div className="home bg-primary-blue">
       <div className="md:container mx-auto px-4 py-3 text-white">
-        <header className="home-header flex justify-between items-center text-sm">
+        <header className="flex justify-between items-center text-sm">
           <p className="font-bold flex gap-1 items-center">
             <i className="fa-solid fa-chevron-left" />
             RysthCraft
@@ -15,6 +16,15 @@ function Home() {
             <i className="fa-solid fa-gear" />
           </div>
         </header>
+        <div className="py-5 grid grid-cols-2">
+          <span>
+            <img className="" src={Logo} alt="" />
+          </span>
+          <span className="flex flex-col justify-center">
+            <h2 className="text-2xl uppercase">Games</h2>
+            <p className="text-sm">2,438 Views</p>
+          </span>
+        </div>
       </div>
     </div>
   );
